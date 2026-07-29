6ce1153732ca989494c45159304dd9b369d1f03b
Key7052 <ismatiq2012@gmail.com>
2026-07-30 02:34:36 +0600

    Change esrb value based on checkbox state
    Updated the esrb variable to use a new value based on the checkbox state.

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const days = 26
 const pl = 144.012846338119000
 const fhrb = document.getElementById("fhrb").checked ? 0.978 : 1
 const emg = document.getElementById("emg").checked ? 0.95 : 1
-const esrb = document.getElementById("esrb").checked ? 0.6025425 : 0
+const esrb = document.getElementById("esrb").checked ? 0.6025425 : 0.2875
 const ascensionGoal = Number(document.getElementById("ascensionGoal").value) * 10**60
 
 let costs = [15, 100, 1_100, 12_000, 130_000, 1_400_000, 20_000_000, 330_000_000, 5_100_000_000, 75_000_000_000, 
